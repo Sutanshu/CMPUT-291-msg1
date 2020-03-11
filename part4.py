394b7d67f42965e8723bff601c19a6a57e6dc3b6
ryanjacquess <r.jacques111@gmail.com>
2020-03-10 21:10:09 -0600

    Update part4.py

--- part4.py
+++ part4.py
@@ -1,14 +1,14 @@
-from datetime import datetime
 import sqlite3
+from datetime import datetime
 
-sid1 = '002'  # temp value
-name1 = 'Ryan'  # temp value
+sid = '002'  # temp value
+name = 'Ryan'  # temp value
 print('Post a sale!')
 op = input('Do you want to enter a product ID? y/n:')
 if op == 'y':
-    pid1 = input('Product ID:')
+    pid = input('Product ID:')
 else:
-    pid1 = None
+    pid = None
 edate = datetime.strptime(input('Sale end date and time:'), '%Y-%m-%d').date()
 descr = input('Sale description:')
 cond = input('Condition:')
@@ -18,13 +18,7 @@ if op == 'y':
 else:
     rprice = None
 
-    
-    
-# for some reason, I have been unable to insert variables into the SQL command, my implementation is below. I am not sure what is going wrong, it throws a syntax error with the %, maybe something is off with my IDE
 if edate > datetime.now().date():
-    '''
-    sql_command = ("""INSERT INTO sales
-                   VALUES (%s, %s, %s, %s, %s, %s, %s)""")
-    var_tuple = (sid, name, pid, edate, descr, cond, rprice)
-    cursor.execute(sql_command, var_tuple)
-    '''
+    sql_command = """INSERT INTO sales
+                   VALUES (?, ?, ?, ?, ?, ?, ?)"""
+    cursor.execute(sql_command, (sid, name, pid, edate, descr, cond, rprice))
