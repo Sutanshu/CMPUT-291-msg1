77278c93dbd7d4c55017b3f09b36d9abbbf747d1
ryanjacquess <r.jacques111@gmail.com>
2020-03-11 16:15:00 -0600

    Update assignment3.py

--- assignment3.py
+++ assignment3.py
@@ -273,9 +273,61 @@ def function2():
 def function3():
     pass
 
-
 def function4():
+    sql_command = """DROP TABLE IF EXISTS users_search"""
+    cursor.execute(sql_command)
+
+    keyword = input('Search for user using keyword:')
+
+    # not sure what the issue is here, ideas?
+    # it works with a hardcoded keyword instead of the '?' placeholder
+    sql_command = """CREATE TABLE users_search AS SELECT email, name, city FROM users WHERE name LIKE '%?%' OR email LIKE '%?%';"""
+    cursor.execute(sql_command, (keyword, keyword))
+
+    # to test and confirm the output is correct
+    sql_command = """SELECT * FROM users_search;"""
+    cursor.execute(sql_command)
+    rows = cursor.fetchall()
+    print(rows)
+
+
+    # part (a)
+    reviewer_temp = 'Ryan'  # temp value
+    reviewee_temp = 'Josh'  # temp value
+    rtext = input('Please write your review:')
+    rating = int(input('What is your rating 1-5?'))
+
+    sql_command = """INSERT INTO reviews
+                     VALUES (?, ?, ?, ?, ?)"""
+    cursor.execute(sql_command, (reviewer_temp, reviewee_temp, rtext, rating, datetime.now().date()))
+
+
+    # part (b)
+
+
+    # part (c)
+    list_reviews(UserEmail)
     pass
+    
+def list_reviews(seller):
+    """
+    List all reviews of the seller. 
+    """
+
+    global connection, cursor
+    print(seller)
+
+    query = """
+            SELECT rtext 
+            FROM reviews
+            WHERE reviewee=?;
+            """
+    cursor.execute(query, (seller, ))
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row[0])
+
+    return
 
 
 def interface():
