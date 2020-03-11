8c9931f682293f00bd4ceb9a27f889ede1ab62ab
Vyome Agarwal <43073294+vyomea@users.noreply.github.com>
2020-03-11 09:25:40 -0600

    Better formatting

--- search_for_sales.py
+++ search_for_sales.py
@@ -41,13 +41,22 @@ def search_for_sales():
     repeat = True
     active_sales_with_descr = []
     active_sales = []
+
+    # Get active sales and then from those active sales choose what sales descriptions have the keywords provided by the user
     while repeat:
         usr_input = input("Please enter some keywords, separted by a single space, to seach for sales: ")
         keywords = set(usr_input.split())
 
         active_sales_with_descr = []
 
-        cursor.execute(get_active_sales())
+        query_get_active_sales = """
+            select s.sid, s.descr, max (amount), s.rprice, cast(julianday(s.edate)-julianday('now') as int)
+            from sales s left outer join bids b using (sid)
+            where s.edate > datetime('now')
+            group by sid, s.descr, s.edate;
+            """
+
+        cursor.execute(query_get_active_sales)
         active_sales = cursor.fetchall()
 
         for active_sale in active_sales:
@@ -60,6 +69,7 @@ def search_for_sales():
 
             if num_matches > 0:
                 active_sales_with_descr.append((num_matches, active_sale))
+                
         
         active_sales_with_descr.sort(key=lambda x: x[0], reverse=True)
 
@@ -68,14 +78,50 @@ def search_for_sales():
         else:
             print("Those keywords did not return any results. Try again")
     
+    # Display the active sales with the keywords
+    print("+" + "-"*25 + "+" + "-"*25 + "+" + "-"*25 + "+" + "-"*10 + "+")
+    print("{0:26}|{1:25}|{2:25.3}|{3}".format("Number", "Description", "Max bid", "Number of days remaining"))
+    print("+" + "-"*25 + "+" + "-"*25 + "+" + "-"*25 + "+" + "-"*10 + "+")
     for sale in active_sales_with_descr:
         amt = sale[1][2]
         if amt == None:
             amt = sale[1][3]
-        print(str(active_sales_with_descr.index(sale) + 1) + ". " + sale[1][1] + "| " + str(amt) + "| " + str(sale[1][4]))
+        print("{0:<26}|{1:25}|{2:<25.3f}|{3}".format(active_sales_with_descr.index(sale) + 1, sale[1][1], amt, sale[1][4]))
+        #print(str(active_sales_with_descr.index(sale) + 1) + ". " + sale[1][1] + "| " + str(amt) + "| " + str(sale[1][4]))
     
     repeat = True
 
+    # Asks the user to choose a sale to display more information about it
+    row, sale_chosen = choose_sale_more_info(active_sales_with_descr)
+
+    # display more info
+    max_amt = row[6]
+
+    # After a sale is selected
+    choose_action_for_sale(sale_chosen, max_amt, row)
+
+def choose_action_for_sale(sale_chosen, max_amt, row):
+    repeat = True
+    while repeat:
+        action = input("1. Place a bid on the selected sale\n2.List all active sales of the seller\n3.List all reviews of the seller\n\nChoose action: ")
+        repeat = False
+
+        if action == "1":
+            place_bid(sale_chosen[1][0], max_amt)
+        elif action == "2":
+            list_active_sales_of_seller(row[0])
+        elif action == "3":
+            list_reviews(row[0])
+        else:
+            print("Please choose a valid option")
+            repeat = True
+
+
+def choose_sale_more_info(active_sales_with_descr):
+    repeat = True
+    print(active_sales_with_descr)
+
+    # Asks the user to choose a sale to display more information about it
     while repeat:
         try:
             num = int(input("Please choose a sale (number on the left) to view more information about it: "))
@@ -90,37 +136,35 @@ def search_for_sales():
                 print("Please emter a valid number")
             else:
                 repeat = False
-
-
-    # display more info
     sale_chosen = active_sales_with_descr[num - 1]
-    print(sale_chosen[1][0])
+    row = show_more_info(sale_chosen)
+
+    return row, sale_chosen
+
+
+def show_more_info(sale_chosen):
+
+    query_more_info = """
+            SELECT lister, COUNT(*), AVG(rating), descr, edate, cond, MAX(amount), rprice
+            FROM sales, reviews, bids
+            WHERE lister = reviewee AND sales.sid =?
+            GROUP BY lister, descr, edate;"""
     
-    cursor.execute(get_more_info(), (sale_chosen[1][0], ))
+    cursor.execute(query_more_info, (sale_chosen[1][0], ))
+
     row = cursor.fetchone()
-    print(row)
+    print("Lister: " + row[0])
+    print("Number of reviews:", str(row[1]))
+    print("Average rating:", str(row[2]))
+    print("Sale description:", row[3])
+    print("End date: ", row[4])
+    print("Condition:", row[5])
+    print("Max bid or reserved price:", row[6])
     print("MAX BID: " + str(row[6]))
-    max_amt = row[6]
     print()
     
-    # After a sale is selected
-    repeat = True
-    while repeat:
-        action = input("1. Place a bid on the selected sale\n2.List all active sales of the seller\n3.List all reviews of the seller\n\nChoose action: ")
-        repeat = False
-
-        if action == "1":
-            place_bid(sale_chosen[1][0], max_amt)
-            
-        elif action == "2":
-            list_active_sales_of_seller(row[0])
-        elif action == "3":
-            list_reviews(row[0])
-        else:
-            print("Please choose a valid option")
-            repeat = True
-
-
+    return row
+    
 def place_bid(sale_id, max_amt_arg):
     """
     Place a bid on the selected sale by entering an amount, and the application should record the bid in the database. 
@@ -148,6 +192,9 @@ def place_bid(sale_id, max_amt_arg):
     insert_bid(sale_id, amt)
 
 def insert_bid(sid, bid_val):
+    """
+    Insert the bid in the datbase
+    """
     global connection, cursor
     select_all_bids = "SELECT bid FROM bids;"
     cursor.execute(select_all_bids)
@@ -169,10 +216,13 @@ def insert_bid(sid, bid_val):
     connection.commit()
 
 def list_active_sales_of_seller(seller):
+    """
+    List all active sales of the seller. The result should be ordered on the remaining time of the sale with sales to expire earlier appearing first.
+    """
     global connection, cursor
     
     query = """
-            select s.sid, s.descr, max (amount), s.rprice, cast(julianday(s.edate)-julianday('now') as int)
+            select s.sid, s.descr, max(amount), s.rprice, cast(julianday(s.edate)-julianday('now') as int)
             from sales s left outer join bids b using (sid)
             where s.edate > datetime('now') AND lister =?
             group by sid, s.descr, s.edate;
@@ -180,22 +230,25 @@ def list_active_sales_of_seller(seller):
     cursor.execute(query, (seller, ))
     connection.commit()
     rows = cursor.fetchall()
-    print(rows)
-    
+    rows.sort(key=lambda x: x[4])
+
+    print("+" + "-"*25 + "+" + "-"*25 + "+" + "-"*25 + "+" + "-"*10 + "+")
+    print("{0:26}|{1:25}|{2:25.3}|{3}".format("Number", "Description", "Max bid", "Number of days remaining"))
+    print("+" + "-"*25 + "+" + "-"*25 + "+" + "-"*25 + "+" + "-"*10 + "+")
+
+    for row in rows:
+        amt = row[2]
+        if amt == None:
+            amt = row[3]
+        print("{0:<26}|{1:25}|{2:<25.3f}|{3}".format(rows.index(row) + 1, row[1], amt, row[4]))
+
+    choose_sale_more_info(rows)
     return
 
     
-
 def list_reviews(seller):
     """
     List all reviews of the seller. 
-    tables:
-    users(email, name, pwd, city, gender)
-    products(pid, descr)
-    sales(sid, lister, pid, edate, descr, cond, rprice)
-    bids(bid, bidder, sid, bdate, amount)
-    reviews(reviewer, reviewee, rating, rtext, rdate)
-    previews(rid, pid, reviewer, rating, rtext, rdate)
     """
 
     global connection, cursor
@@ -211,46 +264,6 @@ def list_reviews(seller):
     print(rows)
     connection.commit()
     return
-
-
-def get_more_info():
-    """
-    From the result, the user should be able to select a sale, and see more detailed information about the sale including the email of the lister, 
-    the rating of the lister (which includes the number of reviews and the average rating), the sale description, the sale end date and time, the condition, 
-    and the maximum bid or the reserved price (if there is no bid). If the sale is associated to a product, the result will also include the product description and 
-    the product rating, which includes the number of reviews and the average rating if available or a text that the product is not reviewed.
-
-    tables:
-    users(email, name, pwd, city, gender)
-    products(pid, descr)
-    sales(sid, lister, pid, edate, descr, cond, rprice)
-    bids(bid, bidder, sid, bdate, amount)
-    reviews(reviewer, reviewee, rating, rtext, rdate)
-    previews(rid, pid, reviewer, rating, rtext, rdate)
-    
-    """
-
-    return """
-            SELECT lister, COUNT(*), AVG(rating), descr, edate, cond, MAX(amount), rprice
-            FROM sales, reviews, bids
-            WHERE lister = reviewee AND sales.sid =?
-            GROUP BY lister, descr, edate;"""
-    
-
-
-def get_active_sales():
-    """
-    The listing in 1 and 2 would include for each sale, the sale description, the maximum bid (if there is a bid on the item)
-    or the reserved price (if there is no bid on the item), and the number of days, hours and minutes left until 
-    the sale expires.
-    """
-    return  """
-            select s.sid, s.descr, max (amount), s.rprice, cast(julianday(s.edate)-julianday('now') as int)
-            from sales s left outer join bids b using (sid)
-            where s.edate > datetime('now')
-            group by sid, s.descr, s.edate;
-            """
-    
 
 def main():
     global connection, cursor
