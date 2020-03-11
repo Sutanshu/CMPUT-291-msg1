20a86a06d40751be2bb8ca1bcb89464513900f1c
Sutanshu Seth <42788023+Sutanshu@users.noreply.github.com>
2020-03-11 13:07:35 -0600

    Update main.py
    TODO:
    1. Add logout to every functionality -- need everything to be merged for that.
    2. MANAGE SQL INJECTIONS - Have to review code for input validation across every "input" call
    3. Have to order sales in function1()
    4. Need to decide what to do after a user is done with a certain function, do we re-prompt the same function, or the new interface?
    URGENT.

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from login import Login
 import sys
 import sqlite3
 global loginstatus
 import random
-conn = sqlite3.connect('assignment22.db')
+from getpass import getpass
+conn = sqlite3.connect(sys.argv[1])
 c=conn.cursor()
+UserEmail = None
 def checkUserEmail(email,number):
     number = number
     if number == 1:
@@ -30,28 +31,40 @@ def checkUserPassword(password):
     return False
 
 def handleLogin():
+    global UserEmail
     while True:
-        email = input("Please enter your email id: ")
-        validity = checkUserEmail(email,2)
+        UserEmail = input("Please enter your email id: ")
+        validity = checkUserEmail(UserEmail,2)
         if not validity:
             print("Email invalid, please try again")
             continue
         break
     while True:
-        password = input("Please enter your password: ")
+        password = getpass('Please enter your password: ')
         rightPassword = checkUserPassword(password)
         if not rightPassword:
             print("Password invalid, please try again")
             continue
-        loginstatus = True
         break
-    c.execute("select name from users where email = ?",(email,))
+    c.execute("select name from users where email = ?",(UserEmail,))
     for i in c.fetchall():
         print("Welcome! {}".format(i[0]))
 def function1():
-    print("Here are your options: ")
-    conn = sqlite3.connect('assignment22.db')
-    c = conn.cursor()
+    """
+    List products. List all products with some active sales associated to them. For each qualifying product, list the product id, 
+    description, the number of reviews, the average rating and the number of active sales associated to the product. 
+    A sale is active if the sale end date and time has not reached yet. Sort the result in descending order of the number of active sales (
+    i.e., the product with the largest number of active sales is listed on top). 
+    From the product listing, the user should be able to select a product and perform the following actions:
+    1. Write a product review by providing a review text and a rating (a number between 1 and 5 inclusive); 
+    the other fields of a review record should be appropriately filled by the application. In particular, a unique review id is recorded, 
+    the review date is set to the current date and time, and the user should be recorded as the reviewer.
+    2. List all reviews of the product.
+    3. List all active sales associated to the product, ordered based on the remaining time of the sale, with sales to expire earlier appearing first; 
+    """
+
+    global UserEmail
+    print("Here are the products: ")
     c.execute("select distinct products.pid, products.descr,count(rtext) as reviews, round(avg(rating),1) as averagerating \
     from sales,previews,products where sales.edate> datetime('now') and previews.pid = products.pid and sales.pid = products.pid group by products.pid, products.descr,sales.edate")
     array = c.fetchall()
@@ -75,17 +88,17 @@ def function1():
     number = 0
     while True:
         choice = input("Your choice: ")
-        c.execute("select * from products where pid = ?",(choice,))
-        if not c.fetchall():
+        if choice not in actualarray:
             print("Invalid PID, try again.")
             continue
         print("Valid PID! Select one of the options from below: ")
         print("1. To write a review")
         print("2. To list all reviews of {}".format(choice))
         print("3. To list all active sales associated to {}".format(choice))
+        print("4. Logout")
         try:
             number = int(input("Enter a respective number: "))
-            if number>3 or number<=0:
+            if number>4 or number<=0:
                 print("Invalid choice, try again.")
                 continue
         except ValueError:
@@ -109,7 +122,7 @@ def function1():
                 continue
             break
         rid = "R" +str(random.randint(0,9999999))
-        c.execute("Insert into previews (rid,pid, reviewer, rating, rtext, rdate) values (?,?,?,?,?,datetime('now'))",(rid,choice,"ok",rating,review))
+        c.execute("Insert into previews (rid,pid, reviewer, rating, rtext, rdate) values (?,?,?,?,?,datetime('now'))",(rid,choice,UserEmail,rating,review))
         conn.commit()
     if number == 2:
         print("Reviews for {} are: ".format(choice))
@@ -123,28 +136,38 @@ def function1():
         print("Active sales are: ")
         for i in c.fetchall():
             print(i[0])
+    if number == 4:
+        main()
+    
 def handleSignUp():
-    conn = sqlite3.connect('assignment22.db')
-    c = conn.cursor()
+    global UserEmail
     print("You can sign up in a few quick easy steps, please fill in the following prompts.")
     while True:
         userName = input("Your name: ")
         if not userName:
             print("Please enter something as a name")
             continue
+        break
+    while True:
         userEmail = input("Your email ID: ")
         validity = checkUserEmail(userEmail,1)
         if not validity:
             print("Invalid email, try again. ")
             continue
-        userPassword = input("Your password: ")
+        break
+    while True:
+        userPassword = getpass('Your password: ')
         if not userPassword:
             print("Please enter something as a password.")
             continue
+        break
+    while True:
         userCity = input("Your city: ")
         if not userCity:
             print("Please enter something as a city.")
             continue
+        break
+    while True:
         userGender = input("Your gender? (F/M): ")
         try:
             if not userGender in ('f','m','M','F'):
@@ -154,46 +177,33 @@ def handleSignUp():
             print("Something went wrong, please try again.")
             continue
         break
-    #try:
+    c.execute("select email from users where email = ?",(userEmail,))
+    if c.fetchall():
+        print("User already exists, please try again.")
+        while True:
+            try:
+                option = int(input("Do you want to log in? If so press 1, if you want to sign up with a new email, press 2."))
+                if option == 1:
+                    handleLogin()
+                    return
+                if option == 2:
+                    handleSignUp()
+                    break
+                if option >2 or option <=0:
+                    continue
+            except ValueError:
+                print("Please enter only 1 or 2")
+                continue
+
     c.execute("INSERT INTO users (email,name,pwd,city,gender) values (?,?,?,?,?)",(userEmail,userName,userPassword,userCity,userGender,))
     conn.commit()
-    """except:
-        print("Something went wrong... terminating.")"""
-        #sys.exit() #have to handle this better
+    #sys.exit() #have to handle this better
 def function2():
     pass
 def function3():
     pass
 def function4():
-    # my attempt to merge part 4
-    conn = sqlite3.connect('assignment22.db')
-    c = conn.cursor()
-    
-    sid = '002'  # temp value
-    name = 'Ryan'  # temp value
-    print("Post a sale!")
-    op = input("Do you want to enter a product ID? y/n:")
-    if op == 'y':
-        pid = input("Product ID:")
-    else:
-        pid = None
-    edate = datetime.strptime(input("Sale end date:"), '%Y-%m-%d').date()
-    descr = input("Sale description:")
-    cond = input("Condition:")
-    op = input("Do you want to enter a reserved price? y/n:")
-    if op == 'y':
-        rprice = int(input("Reserved price:"))
-    else:
-        rprice = None
-
-    if edate > datetime.now().date():
-        sql_command = """INSERT INTO sales
-                         VALUES (?, ?, ?, ?, ?, ?, ?)"""
-        c.execute(sql_command, (sid, name, pid, edate, descr, cond, rprice))
-    else:
-        print("Error, sale end date is in the past")
-        
-    conn.commit()
+    pass
 def function5():
     pass
 def interface():
@@ -203,10 +213,12 @@ def interface():
     print("3. 1-2 Follow Up")
     print("4. Post a sale")
     print("5. Search for users")
+    print("6. Logout")
+
     while True:
         try:
             functionality = int(input("Please enter your choice: "))
-            if functionality<=0 or functionality>5:
+            if functionality<=0 or functionality>6:
                 print("Please enter a number from 1-5, trying again...")
                 continue
         except ValueError:
@@ -218,8 +230,9 @@ def interface():
 def main():
     while True:
         print("Hello! Welcome to our store, to get started, please login or sign up!")
-        print("Press 1 to log in if you already have an email_id and a password, or press 2 to sign up!")
-        print("Press 3 to exit the program")
+        print("1. Log in if you already have an email_id and a password") 
+        print("2. Sign up!")
+        print("3. Exit the program")
         try:
             choice = int(input("Please enter your choice: "))
         except ValueError:
@@ -238,6 +251,9 @@ def main():
                 function4()
             if functionality == 5:
                 function5()
+            if functionality == 6:
+                #UserEmail = None
+                main()
             break
         if choice == 2:
             handleSignUp()
@@ -255,5 +271,6 @@ def main():
         if choice == 3:
             break
         sys.exit()
-            
-main()
+if __name__ == '__main__':
+    main()
+    conn.close()          
