826345e1260a71af73194f412fe714f8ea5f63e2
ryanjacquess <r.jacques111@gmail.com>
2020-03-10 20:38:26 -0600

    Update part5.py

--- part5.py
+++ part5.py
@@ -1,1 +1,16 @@
 import sqlite3
+import datetime from datetime
+
+sql_command = """DROP TABLE IF EXISTS users_search"""
+cursor.execute(sql_command)
+
+sql_command = """CREATE TABLE users_search AS SELECT email, name, city FROM users WHERE name LIKE '%keyword%' OR email LIKE '%keyword%';"""  # keyword as a placeholder
+cursor.execute(sql_command)
+
+rtext = input('Please write your review:')
+rating = int(input('What is your rating 1-5?'))
+
+sql_command = """INSERT INTO reviews
+                 VALUES (reviewer_temp, reviewee_temp, %s, %s, datetime.now().date())"""
+var_tuple = (rtext, rating)
+cursor.execute(sql_command, var_tuple)
